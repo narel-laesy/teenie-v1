5db6c6542df07e58604f87f6511a891540e52e33
narel-laesy <narellaesy@gmail.com>
2026-05-06 06:41:06 -0700

    Add login function for user authentication

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
 
+    if (user === "esther" && pass === "1234") {
+        window.location.href = "home.html";
+    } else {
+        document.getElementById("message").innerText = "Wrong login details!";
+    }
+}
