8a14d67983f7426c23c6c16574d2e260ad85f6c6
narel-laesy <narellaesy@gmail.com>
2026-05-07 13:51:50 +0300

    Change login username from 'esther' to 'paloma'

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if (user === "esther" && pass === "1234") {
+    if (user === "paloma" && pass === "1234") {
         window.location.href = "home.html";
     } else {
         document.getElementById("message").innerText = "Wrong login details!";
