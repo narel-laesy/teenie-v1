5fdf6a534142b1ba5f5750743ecf9e3d22c6a4b3
narel-laesy <narellaesy@gmail.com>
2026-05-11 16:15:08 +0300

    Change login credentials for user authentication

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if (user === "paloma" && pass === "1234") {
+    if (user === "esther" && pass === "bridget") {
         window.location.href = "home.html";
     } else {
         document.getElementById("message").innerText = "Wrong login details!";
