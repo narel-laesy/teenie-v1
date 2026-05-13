7deead2895b60c73dc80544ce7b8011c5346a22d
narel-laesy <narellaesy@gmail.com>
2026-05-13 13:44:30 +0300

    Implement signup function and update login logic

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+function signup() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "" || pass === "") {
+        document.getElementById("message").innerText = "Fill all fields!";
+        return;
+    }
+
+    localStorage.setItem(user, pass);
+
+    document.getElementById("message").innerText = "Account created!";
+}
+
 function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if (user === "esther" && pass === "bridget") {
+    let savedPassword = localStorage.getItem(user);
+
+    if (savedPassword === pass) {
         window.location.href = "home.html";
     } else {
         document.getElementById("message").innerText = "Wrong login details!";
